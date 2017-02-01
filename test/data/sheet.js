92c696637ffedc87c59915922a70bec983f094c1
Stuart Lees <stuartlees@gmail.com>
2017-02-01 11:53:01 -0500

    Fix listing issues

--- test/data/sheet.js
+++ test/data/sheet.js
@@ -1,12 +1,13 @@
-var sheetData = {
-  "majorDimension": "ROWS",
-  "range": "Sheet1!A1:F999",
-  "values": [
-    ["Name", "Type", "Total"],
-    ["Apple", "Fruit", "0.99"],
-    ["Milk", "Drink", "3.99"],
-    ["Crackers", "Snack", "5.99"]
-  ]
-};
+/* exported sheetData, sheetKey */
 
-var sheetKey = "risesheet_abc";
+var sheetData = {
+    "majorDimension": "ROWS",
+    "range": "Sheet1!A1:F999",
+    "values": [
+    [ "Name", "Type", "Total" ],
+    [ "Apple", "Fruit", "0.99" ],
+    [ "Milk", "Drink", "3.99" ],
+    [ "Crackers", "Snack", "5.99" ]
+    ]
+  },
+  sheetKey = "risesheet_abc";
